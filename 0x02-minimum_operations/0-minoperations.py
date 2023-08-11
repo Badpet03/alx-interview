5c6e1134fcf4d186c361581e5fef73213531c4e2
Badpet03 <Richboyflex15@gmail.com>
2023-08-11 00:46:14 +0000

    interview question

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
-
-
-''' A module that returns the minimum Operations it takes to
-    get to n characters.
-
-    Available operations:
-        copy
-        paste
-'''
+"""
+Defines a function that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
 
 
 def minOperations(n):
-    '''
-    returns the minimum operations to get n H's
-    '''
-    min_operations = 0
-
-    if n <= 1:
-        return min_operations
-
-    for i in range(2, n + 1):
-        while n % i == 0:
-            min_operations += i
-            n /= i
-
-    return min_operations
-
-
-if __name__ == '__main__':
-    from random import randint
-    from time import time
-
-    start_time = time()
-
-    for i in range(10):
-        n = randint(2, 100)
-        print("Min # of operations to reach {} char: {}".
-              format(n, minOperations(n)))
-
-    print(f'==> Program completed in {time() - start_time:.3f}s')
+    """Calculates the fewest number of operations needed to result in
+        exactly n H characters in the file."""
+    sumfactors = 0
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            n //= i
+            sumfactors += i
+        else:
+            i += 1
+    return sumfactors + n if n > 1 else sumfactors
